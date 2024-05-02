9e63e2992a1bb4b0c5706c20cc08e633ae6afc4a
Chuong Pham <minhchuong8197@gmail.com>
2024-05-02 21:11:47 +0700

    commit

---
+++ src/components/Product.jsx
@@ -0,0 +1,28 @@
+import { createContext, useContext, useState } from "react";
+import { CartContext } from "../shop/shopping-cart-context.jsx";
+
+export default function Product({
+  id,
+  image,
+  title,
+  price,
+  description
+}) {
+  const {AddItemToCart} = useContext(CartContext)
+
+  return (
+    <article className="product">
+      <img src={image} alt={title} />
+      <div className="product-content">
+        <div>
+          <h3>{title}</h3>
+          <p className='product-price'>${price}</p>
+          <p>{description}</p>
+        </div>
+        <p className='product-actions'>
+          <button onClick={() => AddItemToCart(id)}>Add to Cart</button>
+        </p>
+      </div>
+    </article>
+  );
+}
